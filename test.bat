5f3102402ab0730b802588eda09904a4e74fb57c
unknown <you@example.com>
2021-04-04 05:26:45 -0700

    deyisiklik

--- test.bat
+++ test.bat
@@ -1,9 +1,8 @@
 cd C:\Users\Administrator\Desktop\Projects
-git.exe checkout dev
 git init
 git remote add origin https://github.com/behruz-code/Projects.git
 git.exe add .
-git.exe commit -am "Son deyisiklik"
+git.exe commit -am "deyisiklik"
 git.exe push origin
 git pull --rebase origin master
 git push origin master
